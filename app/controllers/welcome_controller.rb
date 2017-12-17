106c536ba6da5f912158124a6754f10e74125120
jacquelineasano <jacqueline.y.asano@gmail.com>
2017-12-17 12:50:43 -0200

    created a iteration for repeated urls and moved @websites from views to controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,11 @@
 class WelcomeController < ApplicationController
   def index
+    @websites = [
+      ["http://railsgirls.com", "Rails Girls"],
+      ["https://en.wikibooks.org/wiki/Ruby_Programming", "Wikibooks"],
+      ["http://guides.rubyonrails.org", "Ruby on Rails Guides"],
+    ]
   end
 end
+
+#variables starting with @ (called ‘instance variables’) can be accessed by both the view and the controller
